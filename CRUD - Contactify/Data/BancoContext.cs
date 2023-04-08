c60ec05bc7bbbfe611a19738d85d443e13b4a315
ThauanMelo <thauanamz@gmail.com>
2023-04-07 21:54:24 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD - Contactify/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD___Contactify.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD___Contactify.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {   
+        }
+
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ CRUD - Contactify/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using CRUD___Contactify.Models;
+
+namespace CRUD___Contactify.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
+    }
+}
